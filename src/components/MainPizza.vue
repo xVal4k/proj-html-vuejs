fc8dddc2cd60093286d8bae13accf5da869d98da
xVal4k <cristianbricicaru.cb@gmail.com>
2022-04-04 10:43:45 +0200

    added section 6

--- src/components/MainPizza.vue
+++ src/components/MainPizza.vue
@@ -5,6 +5,7 @@
     <section-3 />
     <section-4 />
     <section-5 />
+    <section-6 />
   </main>
 </template>
 
@@ -16,13 +17,15 @@ import Section2 from './micro/Section2.vue'
 import Section3 from './micro/Section3.vue'
 import Section4 from './micro/Section4.vue'
 import Section5 from './micro/Section5.vue'
+import Section6 from './micro/Section6.vue'
 export default {
   components: {
     Section1,
     Section2,
     Section3,
     Section4,
-    Section5
+    Section5,
+    Section6
   },
 
 }
