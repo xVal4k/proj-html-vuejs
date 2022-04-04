3c42cf8b74b8daad912453b2605bd53d52952778
xVal4k <cristianbricicaru.cb@gmail.com>
2022-04-04 10:02:45 +0200

    section 4 added

--- src/components/MainPizza.vue
+++ src/components/MainPizza.vue
@@ -3,6 +3,7 @@
     <section-1 />
     <section-2 />
     <section-3 />
+    <section-4 />
   </main>
 </template>
 
@@ -12,11 +13,13 @@
 import Section1 from './micro/Section1.vue'
 import Section2 from './micro/Section2.vue'
 import Section3 from './micro/Section3.vue'
+import Section4 from './micro/Section4.vue'
 export default {
   components: {
     Section1,
     Section2,
-    Section3
+    Section3,
+    Section4
   },
 
 }
---
+++ src/components/micro/Section4.vue
@@ -0,0 +1,122 @@
+<template>
+  <section id="sec_4">
+    <div class="row row_top">
+      <div class="col col_left"></div>
+      <div class="col col_center"></div>
+      <div class="col col_right"></div>
+    </div>
+    <div class="row row_middle">
+      <div class="col col_left"></div>
+      <div class="col col_center"></div>
+      <div class="col col_right"></div>
+    </div>
+    <div class="row row_bottom">
+      <div class="col col_left"></div>
+      <div class="col col_center"></div>
+      <div class="col col_right"></div>
+    </div>
+    <h1>
+      GO AHEAD AND <br />
+      BUILD YOUR <br />
+      OWN PIZZA <br />
+      WE WON'T <br>
+      JUSTGE!
+    </h1>
+  </section>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: "Section4",
+};
+</script>
+
+<style scoped lang="scss">
+#sec_4 {
+  margin: 0.5rem 0;
+  position: relative;
+  h1 {
+    position: absolute;
+    top: 50%;
+    left: 20%;
+    color: white;
+    font-size: 3rem;
+    transform: translateY(-50%);
+  }
+  .row {
+    display: flex;
+    .col {
+      background-image: url(../../assets/images/img/h3-background-img.jpg);
+      background-size: auto;
+    }
+    .col_left,
+    .col_right {
+      width: 6.7%;
+      max-width: 128.64px;
+    }
+    .col_center {
+      width: 86.6%;
+      max-width: 1662.72px;
+    }
+  }
+  .row_top,
+  .row_bottom {
+    .col_left,
+    .col_right {
+      &:before {
+        content: "";
+        float: left;
+        padding-top: 100%;
+      }
+    }
+  }
+  .row_middle {
+    .col_left,
+    .col_right {
+      &:before {
+        content: "";
+        float: left;
+        padding-top: 230%;
+      }
+    }
+  }
+  .row_top {
+    .col_left {
+      background-position: 99% 100%;
+    }
+    .col_center {
+      background-position: bottom;
+      background-size: cover;
+    }
+    .col_right {
+      background-position: 2% 100%;
+    }
+  }
+  .row_middle {
+    .col_left {
+      background-position: 100% 4%;
+    }
+    .col_center {
+      background-position: top right;
+      background-size: cover;
+    }
+    .col_right {
+      background-position: 2% 7%;
+    }
+  }
+  .row_bottom {
+    .col_left {
+      background-position: 100% 65%;
+    }
+    .col_center {
+      background-position: 40% 65%;
+      background-size: cover;
+    }
+    .col_right {
+      background-position: 0% 65%;
+    }
+  }
+}
+</style>
