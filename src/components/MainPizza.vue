8817228dc6a39e81f11624b3e0fe3aaeb2b18320
xVal4k <cristianbricicaru.cb@gmail.com>
2022-04-04 10:27:09 +0200

    added section 5 - need fix

--- src/components/MainPizza.vue
+++ src/components/MainPizza.vue
@@ -4,6 +4,7 @@
     <section-2 />
     <section-3 />
     <section-4 />
+    <section-5 />
   </main>
 </template>
 
@@ -14,12 +15,14 @@ import Section1 from './micro/Section1.vue'
 import Section2 from './micro/Section2.vue'
 import Section3 from './micro/Section3.vue'
 import Section4 from './micro/Section4.vue'
+import Section5 from './micro/Section5.vue'
 export default {
   components: {
     Section1,
     Section2,
     Section3,
-    Section4
+    Section4,
+    Section5
   },
 
 }
---
+++ src/components/micro/Section5.vue
@@ -0,0 +1,85 @@
+<template>
+  <section id="sec_5">
+    <div v-for="(photo, index) in workers"
+    :key="index" class="img_cont">
+      <img :src="photo.src" :alt="photo.name" />
+      <div class="hover_eye">
+        <h2>{{ photo.name }}</h2>
+        <p>{{ photo.role }}</p>
+        <p>logo logo logo</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+/* eslint-disable */
+import Mike from "../../assets/images/img/h1-team-1a-700x700.jpg";
+import Sara from "../../assets/images/img/h1-team-2a.jpg";
+import Jimmy from "../../assets/images/img/h1-team-3a.jpg";
+import Gloria from "../../assets/images/img/h1-team-4a.jpg";
+
+export default {
+  name: 'Section5',
+  data() {
+    return {
+      workers: [
+        {
+          name: 'Mike Garcia',
+          role: 'Head Chef',
+          src: Mike,
+        },
+        {
+          name: 'Sara Smith',
+          role: 'Kitchen Porter',
+          src: Sara,
+        },
+        {
+          name: 'Jimmy Martinez',
+          role: 'Chef',
+          src: Jimmy,
+        },
+        {
+          name: 'Gloria Rodriguez',
+          role: 'Kitchen Porter',
+          src: Gloria,
+        },
+      ],
+      imgHover: null,
+    };
+  },
+}
+</script>
+
+<style scoped lang="scss">
+#sec_5 {
+  margin: .5rem 0;
+  display: flex;
+  .img_cont {
+    position: relative;
+    width: calc(100% / 4);
+  }
+  img {
+    width: 100%;
+  }
+  .hover_eye {
+    width: 90%;
+    height: 90%;
+    background: tomato;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    position: absolute;
+    z-index: 9;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    p {
+      font-size: 14px;
+      margin: .25rem 0 .5rem 0;
+    }
+  }
+}
+</style>
